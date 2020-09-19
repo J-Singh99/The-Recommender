344805664a91e11932ae165de5362a264c69d964
Atul_Jawa <48586269+Atul-Jawa@users.noreply.github.com>
2020-09-19 20:32:27 +0530

    Added chat template

--- theRecommender/accounts/urls.py
+++ theRecommender/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
+    path('chat/$', views.chat, name='chat'),
     path('', views.index, name='index'),
 
 ]
--- theRecommender/accounts/views.py
+++ theRecommender/accounts/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html' , {'page':'index'} )
+
+def chat(request):
+    return render(request, 'chat.html' , {'page':'chat'} )
