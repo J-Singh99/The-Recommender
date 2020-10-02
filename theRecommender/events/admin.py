6d08c3905515d4d4a5d0558e8485af831366b382
J-Singh99 <jaspreet099@gmail.com>
2020-10-02 21:42:49 +0530

    Merged all events shiz

--- theRecommender/events/admin.py
+++ theRecommender/events/admin.py
@@ -1,3 +1,2 @@
 from django.contrib import admin
-from .models import eventPosts
 # Register your models here.
--- theRecommender/events/models.py
+++ theRecommender/events/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
 
 class Tag(models.Model):
     club_tag = models.CharField(max_length=3, unique=True,default="DEF")
