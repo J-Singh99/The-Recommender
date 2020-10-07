fecfa71087bba63dad9f279a7a18afde935820fa
Atul_Jawa <48586269+Atul-Jawa@users.noreply.github.com>
2020-10-07 16:32:54 +0530

    Update models.py

--- theRecommender/accounts/models.py
+++ theRecommender/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    #username firstname lastname email password 
+    image      = models.ImageField(upload_to='profile_img', default='profile_img/image.jpg' )
+    university = models.CharField(max_length=100, null=True, blank=True) # # here means teacher kya hai like software engineer is name is correct for thi
+    desciption = models.TextField(null=True, blank=True) 
