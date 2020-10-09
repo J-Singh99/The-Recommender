af056d08abe09b57f7a67184084d73fd1daf3d81
Parijat Garg <parijatacc@gmail.com>
2020-10-09 21:48:48 +0530

    EVENTS - All main functionalities - FEED | ADD | DELETE | REGISTER | MORE

--- theRecommender/events/admin.py
+++ theRecommender/events/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,club,events,AdminOf,RegisteredEvents
 # Register your models here.
 
 admin.site.register(Post)
+admin.site.register(club)
+admin.site.register(events)
+admin.site.register(AdminOf)
+admin.site.register(RegisteredEvents)
