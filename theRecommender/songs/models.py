581e8d9830035f6b700b5a28c6d976e3e2916698
Malvika Jindal <malvikajindal2000@gmail.com>
2020-10-20 00:41:10 +0530

    Added other files for personalized rec.

--- theRecommender/songs/models.py
+++ theRecommender/songs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from accounts.models import User
 # Create your models here.
+
+
+class songInfo(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.CharField(max_length=100, unique=True)
+    listen_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.song
