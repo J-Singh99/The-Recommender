feb10895c5236fc7249c5559be509fdcd1d07536
J-Singh99 <jaspreet099@gmail.com>
2020-10-21 10:45:06 +0530

    Base try 1 Netflix template ready

--- theRecommender/movieRecom/views.py
+++ theRecommender/movieRecom/views.py
@@ -10,9 +10,28 @@ from django.views.generic import (
 )
 from django.contrib import messages
 
+movieInfo = [
+    {'name':'Movie1', 'rel_date':'Today', 'rating':6, 'length':'120 mins', 'Director':'Tarantino', 'random':1},
+    {'name':'Movie2', 'rel_date':'Yesterday', 'rating':4, 'length':'120 mins', 'Director':'Tarantino1', 'random':2},
+    {'name':'Movie3', 'rel_date':'Tomorrow', 'rating':1, 'length':'120 mins', 'Director':'Tarantino2', 'random':3},
+    {'name':'Movie4', 'rel_date':'Today', 'rating':10, 'length':'120 mins', 'Director':'Tarantino3', 'random':4},
+    {'name':'Movie5', 'rel_date':'Yesterday', 'rating':3, 'length':'120 mins', 'Director':'Tarantino4', 'random':5},
+    {'name':'Movie6', 'rel_date':'Tomorrow', 'rating':7, 'length':'120 mins', 'Director':'Tarantino5', 'random':6},
+    {'name':'Movie7', 'rel_date':'Today', 'rating':3, 'length':'120 mins', 'Director':'Tarantino6', 'random':7},
+    {'name':'Movie8', 'rel_date':'Yesterday', 'rating':6, 'length':'120 mins', 'Director':'Tarantino7', 'random':8},
+    {'name':'Movie9', 'rel_date':'Tomorrow', 'rating':9, 'length':'120 mins', 'Director':'Tarantino8', 'random':9},
+    {'name':'Movie10', 'rel_date':'Today', 'rating':7, 'length':'120 mins', 'Director':'Tarantino9', 'random':10},
+    {'name':'Movie11', 'rel_date':'Yesterday', 'rating':2, 'length':'120 mins', 'Director':'Tarantino10', 'random':11},
+    {'name':'Movie12', 'rel_date':'Tomorrow', 'rating':3, 'length':'120 mins', 'Director':'Tarantino11', 'random':12},
+    {'name':'Movie13', 'rel_date':'Today', 'rating':8, 'length':'120 mins', 'Director':'Tarantino12', 'random':13}
+]
+
+context = {
+        'movieInfo': movieInfo
+    }
 
 def test(request):
-	return render(request, 'movieBASE.html')
+	return render(request, 'movieBASE.html', context = context)
 
 
 '''
